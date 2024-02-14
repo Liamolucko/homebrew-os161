7a0ced771fc4287e4958a143811f7f7456847fe1
Liam Murphy <liampm32@gmail.com>
2024-02-14 20:22:31 +1100

    Get GDB working

--- Formula/os161-gdb.rb
+++ Formula/os161-gdb.rb
@@ -16,7 +16,6 @@ class Os161Gdb < Formula
       "--with-gdb-datadir=#{share}/mips-harvard-os161-gdb",
       "--disable-debug",
       "--disable-dependency-tracking",
-      "--with-python=/usr",
       "--without-lzma",
       "--target=mips-harvard-os161",
     ]
@@ -43,7 +42,7 @@ index 73117f3..de842c9 100644
 @@ -60,22 +60,26 @@ extern void sim_addr_range_delete (ADDR_RANGE * /*ar*/,
            address_word /*start*/,
            address_word /*end*/);
- 
+
 +/* TODO: This should get moved into sim-inline.h.  */
 +#ifdef HAVE_INLINE
 +#ifdef SIM_ARANGE_C
@@ -62,7 +61,7 @@ index 73117f3..de842c9 100644
  sim_addr_range_hit_p (ADDR_RANGE * /*ar*/, address_word /*addr*/);
  #define ADDR_RANGE_HIT_P(ar, addr) \
    ((ar)->range_tree == NULL || sim_addr_range_hit_p ((ar), (addr)))
- 
+
  #ifdef HAVE_INLINE
 -#ifdef SIM_ARANGE_C
 -#define SIM_ARANGE_INLINE INLINE
@@ -74,7 +73,7 @@ index 73117f3..de842c9 100644
 -#define SIM_ARANGE_INLINE
  #endif
  #define SIM_ARANGE_C_INCLUDED
- 
+
 diff --git a/sim/common/sim-inline.h b/sim/common/sim-inline.h
 index af75562..8a9c286 100644
 --- a/sim/common/sim-inline.h
@@ -90,3 +89,48 @@ index af75562..8a9c286 100644
  #define INLINE2 __inline__
  #else
  #define INLINE2 /*inline*/
+
+diff --git a/readline/rltty.c b/readline/rltty.c
+index d237b1c..29cdbb2 100644
+--- a/readline/rltty.c
++++ b/readline/rltty.c
+@@ -37,9 +37,7 @@
+
+ #include "rldefs.h"
+
+-#if defined (GWINSZ_IN_SYS_IOCTL)
+-#  include <sys/ioctl.h>
+-#endif /* GWINSZ_IN_SYS_IOCTL */
++#include <sys/ioctl.h>
+
+ #include "rltty.h"
+ #include "readline.h"
+
+diff --git a/readline/terminal.c b/readline/terminal.c
+index 8094186..cd99438 100644
+--- a/readline/terminal.c
++++ b/readline/terminal.c
+@@ -51,9 +51,7 @@
+ /* System-specific feature definitions and include files. */
+ #include "rldefs.h"
+
+-#if defined (GWINSZ_IN_SYS_IOCTL) && !defined (TIOCGWINSZ)
+-#  include <sys/ioctl.h>
+-#endif /* GWINSZ_IN_SYS_IOCTL && !TIOCGWINSZ */
++#include <sys/ioctl.h>
+
+ #ifdef __MSDOS__
+ # include <pc.h>
+
+diff --git a/sim/common/gentmap.c b/sim/common/gentmap.c
+index 7efc6d3..1769c53 100644
+--- a/sim/common/gentmap.c
++++ b/sim/common/gentmap.c
+@@ -2,6 +2,7 @@
+
+ #include <stdio.h>
+ #include <stdlib.h>
++#include <string.h>
+
+ struct tdefs {
+   char *symbol;
